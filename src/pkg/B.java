e1eca5406b3a9fbdaea0453fe0975e1f75a3c165
Hoan Nguyen <hoanamzn@amazon.com>
2020-01-23 17:06:13 -0800

    add B

---
+++ src/pkg/B.java
@@ -0,0 +1,12 @@
+package pkg;
+
+class B {
+
+    void m() {
+        emitChainDensitySpecifiedMetrics(operationFailedWith500,
+                    latency,
+                    t.getClass().equals(com.amazon.awscanal.exception.ThrottlingException.class),
+                    metrics);
+    }
+
+}
